bfb2e9a004cfc590f929cbe40712949df105b76b
AngelinaRush <Linochka_1999@mail.ru>
2023-05-19 01:26:23 +0300

    style: small changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.left}>
-        <NavLink className={styles.logo} to={'/vacations'}>
+        <NavLink className={styles.logo} to={'/'}>
           <div className={styles.logoImg}>
             <Logo />
           </div>
