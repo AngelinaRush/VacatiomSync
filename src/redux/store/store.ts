e6fd45e999a367de0f80221e16b18c32822fdaf6
AngelinaRush <Linochka_1999@mail.ru>
2023-05-18 17:17:14 +0300

    hide logger

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,9 +1,13 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, legacy_createStore as createStore, Middleware } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
 import commonReducer from '../rootReducer'
 
-const store = createStore(commonReducer, applyMiddleware(thunk, logger))
+const REACT_APP_USE_REDUX_LOGGER = process.env.REACT_APP_USE_REDUX_LOGGER
+
+const middlewares = [thunk, REACT_APP_USE_REDUX_LOGGER && logger].filter(Boolean) as Middleware<any, any, any>[]
+
+const store = createStore(commonReducer, applyMiddleware(...middlewares))
 
 export default store
